bf839a41ecd332327f33c067fdcdf01f0c337135
Wisnu Widianto <wisnuprsj@gmail.com>
2023-02-16 02:41:32 +0700

    modify dockerfile and add conf config for nginx

---
+++ library-management/frontend/angular/tailwind.config.js
@@ -0,0 +1,48 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  darkMode: "class",
+  theme: {
+    fontFamily: {
+      display: ["Open Sans", "sans-serif"],
+      body: ["Open Sans", "sans-serif"],
+      text: ["Inter"],
+    },
+    extend: {
+      fontSize: {
+        14: "14px",
+      },
+      backgroundColor: {
+        "main-bg": "#FAFBFB",
+        "secondary-bg": "#8b9999",
+        "main-dark-bg": "#20232A",
+        "secondary-dark-bg": "#33373E",
+        "light-gray": "#CFCFCF",
+        "light-black": "#F7F7F7",
+        "half-transparent": "rgba(0, 0, 0, 0.5)",
+      },
+      borderWidth: {
+        1: "1px",
+      },
+      borderColor: {
+        color: "rgba(0, 0, 0, 0.1)",
+      },
+      width: {
+        400: "400px",
+        760: "760px",
+        780: "780px",
+        800: "800px",
+        1000: "1000px",
+        1200: "1200px",
+        1400: "1400px",
+      },
+      height: {
+        80: "80px",
+      },
+      minHeight: {
+        590: "590px",
+      },
+    },
+  },
+  plugins: [],
+};
