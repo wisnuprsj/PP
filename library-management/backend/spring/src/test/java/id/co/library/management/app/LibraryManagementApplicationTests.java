e4352183ae27d93b0e119d57b751450ed341a081
Wisnu Widianto <wisnuprsj@gmail.com>
2023-02-15 23:47:15 +0700

    initial commit and scaffolding entire application

---
+++ library-management/backend/spring/src/test/java/id/co/library/management/app/LibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.library.management.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
